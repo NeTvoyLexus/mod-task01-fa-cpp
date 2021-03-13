e2de3230718cde8ec3943ae8214af8070c9da4db
NeTvoyLexus <0soulkey0@gmail.com>
2021-03-13 15:44:22 +0300

    1 commit

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,12 +1,82 @@
 unsigned int faStr1(const char *str)
 {
-    return 0;
+    int i = 0;
+    int k = 0;
+    bool flag = 0;
+    bool l = 0;
+    while(str[i] != '\0')
+    {
+        if(str[i] != ' ' && !flag)
+        {
+            flag = 1;
+            l = 1;
+        }
+        if(str[i] == ' ' && flag)
+        {
+            flag = 0;
+            if(l==1)
+                k++;
+        }
+        if(flag && str[i] > 47 && str[i] < 58)
+            l = 0;
+        i++;
+    }
+    if(str[i - 1] != ' ')
+        if(l)
+            k++;
+    return k;
 }
 unsigned int faStr2(const char *str)
 {
-    return 0;
+    int i = 0;
+    int k = 0;
+    bool flag = 0;
+    bool l = 0;
+    while(str[i] != '\0')
+    {
+        if(str[i] != ' ' && !flag)
+        {
+            flag = 1;
+            if(str[i] > 64 && str[i] < 91)
+            {
+                l = 1;
+                i++;
+            }
+        }
+        if(str[i] == ' ' && flag)
+        {
+            flag = 0;
+            if(l)
+                k++;
+        }
+        if( flag && (str[i] < 97 || str[i] > 122) )
+            l = 0;
+        i++;
+    }
+    if(str[i - 1] != ' ')
+        if(l)
+            k++;
+    return k;
 }
 unsigned int faStr3(const char *str)
 {
-    return 0;
+    int k=0;
+    int i=0;
+    bool flag=0;
+    double sum=0.0;
+    while(str[i]!='\0'){
+        if(str[i] !=' ' && !flag){
+            k++;
+            flag=1;
+        }
+        else if (str[i] !=' ' && flag)
+                sum++;
+        else if (str[i] !=' ')
+                flag = 0;
+        i++;
+    }
+    sum= (sum+k)/k;
+    int sum1=sum;
+    if(sum-sum1 >= 0.5)    sum1++;
+    return sum1;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-#include "fun.h"
-
+#include "../include/fun.h"
+#include "fun.cpp"
+#include <iostream>
 int main()
 {
     return 0;
